3604308e2d639a45e21aeda43766a17e3dd22f8f
Benjamin Stigsen <benjamin.stigsen@gmail.com>
2020-05-30 20:00:28 +0200

    Update fibonacci_in_O(1)_time.py
    - Fixes errors when trying to find an uneven fibonacci number. E.g. before fib(7) would return 12, even though the correct answer is 13.
    - This change also removes the math module (this is both an improvement in performance and makes it so that it doesn't have any dependencies). (This is just personal taste though)

--- fibonacci_in_O(1)_time.py
+++ fibonacci_in_O(1)_time.py
@@ -1,9 +1,12 @@
-import math
 """
-Find fibonacci numbers in O(1) time, can be shortened to one liner but extended for readabilaty
-The one liner would be math.pow(((1 + math.sqrt(5)) / 2), tofind) / math.sqrt(5)
+Find fibonacci numbers in ~O(1) time, can be shortened to one liner but extended for readabilaty
 """
+
 def fib(tofind):
-    phi = ((1 + math.sqrt(5)) / 2)
-    return math.floor(math.pow(phi, tofind) / math.sqrt(5))
-print(fib(110))
+	phi = (1 + (5 ** 0.5)) / 2
+	if tofind % 2 == 0:
+		return int((phi ** tofind) / (5 ** 0.5))
+	else:
+		return int(((phi ** tofind) / (5 ** 0.5)) + 1)
+
+fib(110)
