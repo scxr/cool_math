9d2d5e63c76808264f5e55fc8d36661161022332
cswil <56652842+cswil@users.noreply.github.com>
2020-06-08 23:59:29 +0100

    Update fifth_root_trick.py

--- tricks/fifth_root_trick.py
+++ tricks/fifth_root_trick.py
@@ -1,6 +1,8 @@
 def trick_main(number):
     result = None
     temp_cnt = 10
+    if number < 100_000:
+        return int(str(number)[len(str(number))-1])
     if number == 100_000:
         return 10
     while result is None:
@@ -12,17 +14,18 @@ def trick_main(number):
     return result
 
 def tests():
-    assert trick_main(100_000) == 10, 'should be 10'
-    assert trick_main(161_051) == 11, 'should be 11'
-    assert trick_main(371_293) == 13, 'should be 13'
-    assert trick_main(1_048_576) == 16, 'should be 16'
-    assert trick_main(3_200_000) == 20, 'should be 20'
-    assert trick_main(9_765_625) == 25, 'should be 25'
-    assert trick_main(28_629_151) == 31, 'should be 31'
-    assert trick_main(79_235_168) == 38, 'should be 38'
-    assert trick_main(205_962_976) == 46, 'should be 46'
-    assert trick_main(5_392_186_096_32) == 222, 'should be 222'
-    assert trick_main(58_797_610_148_157) == 877, 'should be 877'
+    assert trick_main(32) == 2, 'should be 10'
+    assert trick_main(100000) == 10, 'should be 10'
+    assert trick_main(161051) == 11, 'should be 11'
+    assert trick_main(371293) == 13, 'should be 13'
+    assert trick_main(1048576) == 16, 'should be 16'
+    assert trick_main(3200000) == 20, 'should be 20'
+    assert trick_main(9765625) == 25, 'should be 25'
+    assert trick_main(28629151) == 31, 'should be 31'
+    assert trick_main(79235168) == 38, 'should be 38'
+    assert trick_main(205962976) == 46, 'should be 46'
+    assert trick_main(539218609632) == 222, 'should be 222'
+    assert trick_main(518797610148157) == 877, 'should be 877'
 
 
 
